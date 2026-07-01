6fa4a8753597bdf8dd7c4d8b2353c9a2d76acdd3
Amit Gupta <gupta-amit>
2026-07-01 20:00:04 +0530

    minor fix

--- nautobot_vlan_request/urls.py
+++ nautobot_vlan_request/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import VLANRequestListView, VLANRequestEditView
+from .views import VLANRequestListView, VLANRequestEditView, VLANRequestDetailView
 
 urlpatterns = [
     path(
