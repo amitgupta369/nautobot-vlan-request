4ae75064d1913620ee6d55d2ed48732552791b85
Amit Gupta <gupta-amit>
2026-07-01 17:53:22 +0530

    Minor Fix

--- nautobot_vlan_request/tables.py
+++ nautobot_vlan_request/tables.py
@@ -1,8 +1,8 @@
-import django_tables2 as tables
+from nautobot.apps.tables import BaseTable
 from .models import VLANRequest
 
 
-class VLANRequestTable(tables.Table):
+class VLANRequestTable(BaseTable):
     class Meta:
         model = VLANRequest
         fields = (
